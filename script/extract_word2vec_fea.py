43e9e025d1924c9e811c7d3e3be99ffd55280853
swdanielli <swdanielli@github.com>
2016-01-19 17:02:52 -0500

    commit

--- script/extract_word2vec_fea.py
+++ script/extract_word2vec_fea.py
@@ -6,6 +6,7 @@ nlp_util_dir = '/usr/users/swli/program/nlp_util'
 import gensim, logging
 import math
 import numpy as np
+import os.path
 import re
 from scipy import spatial
 import sys
@@ -96,14 +97,31 @@ def get_tx_docs(corpus, corpus_pos, pos_type, tx_list_name):
   for line in open(tx_list_name):
     l_id = re.match('C(\d+_\d+).html\t', line).group(1)
     doc_tag = get_doc_tag(tx_doc_prefix, l_id)
-    docs.append(filter_doc_by_pos(corpus[doc_tag], corpus_pos[doc_tag], pos_type))
+
+    docs.append([])
+    for sentence_index, sentence in enumerate(corpus[doc_tag]):
+      docs[-1].append(filter_doc_by_pos(sentence, corpus_pos[doc_tag][sentence_index], pos_type))
   return docs
 
-def load_corpus(corpus_name):
+def load_corpus(corpus_name, pool_param=None):
   corpus = {}
   for line in open(corpus_name):
     items = line.strip().split('\t')
-    corpus[items[1]] = items[0].split(' ')
+    if tx_doc_prefix in items[1]:
+      corpus[items[1]] = []
+      sentences = re.split(' <br> ', items[0])
+      for sentence in sentences:
+        words = re.split(' ', sentence)
+        if pool_param and len(words) < pool_param[2]: # sentence shorter than threshold
+          corpus[items[1]].append([])
+        else:
+          corpus[items[1]].append(words)
+    else:
+      words = re.split(' ', items[0])
+      if pool_param and len(words) < pool_param[2]: # sentence shorter than threshold
+        corpus[items[1]] = []
+      else:
+        corpus[items[1]] = words
   return corpus
 
 def windowing(docs_temp, window_size):
@@ -117,7 +135,7 @@ def windowing(docs_temp, window_size):
     docs.append(doc)
   return docs
 
-def window_by_seg(docs_temp, seg_filename):
+def window_by_seg(docs_temp, seg_filename, pool_param=None):
   docs = []
   prev_label = '0'
   doc_index = 0
@@ -126,7 +144,10 @@ def window_by_seg(docs_temp, seg_filename):
     if label != prev_label:
       docs.append([])
       prev_label = label
-    docs[-1] += docs_temp[doc_index][:]
+    if pool_param:
+      docs[-1].append(docs_temp[doc_index][:])
+    else:
+      docs[-1] += docs_temp[doc_index][:]
     doc_index += 1
   return docs
 
@@ -140,6 +161,10 @@ def get_one_hot_vec(doc, stop_list, oov_dict):
   return arr
 
 def get_words2vecs(doc, model, dim, stop_list, is_normalization_word=False, fix_stem=None):
+  # flatten doc: doc[sentence_id][word_id] -> doc[word_id]
+  if len(doc) > 0 and isinstance(doc[0], list):
+    doc = sum(doc, [])
+
   if fix_stem:
     (refined_oov, stem_map, oov_map) = fix_stem
     doc_oov_refined = []
@@ -199,7 +224,68 @@ def compute_cos_sim_one_hot(doc_1, doc_2, stop_list, oov_dict):
   cos_sim = 1 - spatial.distance.cosine(vec_1, vec_2)
   return cos_sim if not math.isnan(cos_sim) else 0
 
-def compute_cos_sim(doc_1, doc_2, model, dim, stop_list, is_normalization=False, is_normalization_word=False, fix_stem=None):
+'''
+  trans X tx
+'''
+def compute_pool_cos_sim(doc_1, doc_2, model, dim, stop_list, fix_stem, pool_param, is_normalization=False, is_normalization_word=False):
+  words2vecs_1, words2vecs_2 = [], []
+  for sentence in doc_1:
+    words2vecs_1.append(get_words2vecs(sentence, model, dim, stop_list, is_normalization_word, fix_stem))
+  for sentence in doc_2:
+    words2vecs_2.append(get_words2vecs(sentence, model, dim, stop_list, is_normalization_word, fix_stem))
+
+  scores = [[0.0 for _ in range(len(words2vecs_2))] for _ in range(len(words2vecs_1))]
+  for index_1, words2vec_1 in enumerate(words2vecs_1):
+    for index_2, words2vec_2 in enumerate(words2vecs_2):
+      if words2vec_1.shape[0] == 0 or words2vec_2.shape[0] == 0:
+        continue
+      doc2vec_1 = get_doc2vec(words2vec_1, is_normalization)
+      doc2vec_2 = get_doc2vec(words2vec_2, is_normalization)
+      scores[index_1][index_2] = 1 - spatial.distance.cosine(doc2vec_1, doc2vec_2)
+
+  cos_sims = []
+  for _ in range(min(len(words2vecs_1), len(words2vecs_2), pool_param[1])):
+    arr = np.array(scores)
+    if 'max_n' in pool_param[0]:
+      score = arr.max()
+    elif 'median' in pool_param[0]:
+      score = np.percentile(arr, 50, interpolation='lower')
+    elif 'median_median' in pool_param[0]:
+      score = np.percentile(np.percentile(arr, 50, axis=1, interpolation='lower'), 50, axis=0, interpolation='lower')
+    elif 'median_max' in pool_param[0]:
+      score = np.percentile(arr, 50, axis=1, interpolation='lower').max()
+    elif '90th_90th' in pool_param[0]:
+      score = np.percentile(np.percentile(arr, 90, axis=1, interpolation='lower'), 90, axis=0, interpolation='lower')
+    elif '90th_max' in pool_param[0]:
+      score = np.percentile(arr, 90, axis=1, interpolation='lower').max()
+    elif 'second_second' in pool_param[0]:
+      score = np.percentile(np.percentile(arr, 99.999, axis=1, interpolation='lower'), 99.999, axis=0, interpolation='lower')
+    elif 'second_max' in pool_param[0]:
+      score = np.percentile(arr, 99.999, axis=1, interpolation='lower').max()
+
+    cos_sims.append(score)
+    score_index_1, score_index_2 = np.where(arr == score)
+    for index_1 in range(len(words2vecs_1)):
+      scores[index_1][score_index_2[0]] = 0.0
+    for index_2 in range(len(words2vecs_2)):
+      scores[score_index_1[0]][index_2] = 0.0
+
+  return sum(cos_sims)/len(cos_sims) if cos_sims else 0.0
+
+def compute_cos_sim(doc_1, doc_2, model, dim, stop_list, is_normalization=False, is_normalization_word=False, fix_stem=None, pool_param=None):
+  if pool_param:
+    return compute_pool_cos_sim(
+      doc_1,
+      doc_2,
+      model,
+      dim,
+      stop_list,
+      fix_stem,
+      pool_param,
+      is_normalization=is_normalization,
+      is_normalization_word=is_normalization_word
+    )
+
   words2vecs_1 = get_words2vecs(doc_1, model, dim, stop_list, is_normalization_word, fix_stem)
   words2vecs_2 = get_words2vecs(doc_2, model, dim, stop_list, is_normalization_word, fix_stem)
 
@@ -266,7 +352,7 @@ def normalize_cos_sim(cos_sims, is_normalization_cos_sim):
       cos_sims[:, c] = (cos_sims[:, c]-mean(cos_sims[:, c]))/std(cos_sims[:, c])
   return cos_sims
 
-def print_fea(filename, trans, targets, model, dim, stop_list, is_normalization_doc2vec, is_normalization_cos_sim, oov_dict=None, is_normalization_word=False, fix_stem=None):
+def print_fea(filename, trans, targets, model, dim, stop_list, is_normalization_doc2vec, is_normalization_cos_sim, oov_dict=None, is_normalization_word=False, fix_stem=None, pool_param=None):
   f_o = open(filename, 'w')
   f_cos_sims = []
   for trans_doc in trans:
@@ -275,7 +361,7 @@ def print_fea(filename, trans, targets, model, dim, stop_list, is_normalization_
       if oov_dict:
         f_cos_sim.append(compute_cos_sim_one_hot(trans_doc, target_doc, stop_list, oov_dict))
       else:
-        f_cos_sim.append(compute_cos_sim(trans_doc, target_doc, model, dim, stop_list, is_normalization=is_normalization_doc2vec, is_normalization_word=is_normalization_word, fix_stem=fix_stem))
+        f_cos_sim.append(compute_cos_sim(trans_doc, target_doc, model, dim, stop_list, is_normalization=is_normalization_doc2vec, is_normalization_word=is_normalization_word, fix_stem=fix_stem, pool_param=pool_param))
     f_cos_sims.append(f_cos_sim)
   f_cos_sims = normalize_cos_sim(f_cos_sims, is_normalization_cos_sim)
 
@@ -286,22 +372,32 @@ def print_fea(filename, trans, targets, model, dim, stop_list, is_normalization_
 def assign_argv(argv, argv_index, assign_fn=lambda x: x):
   return (assign_fn(argv[argv_index]), argv_index+1)
 
+def gen_pool_param(is_pool):
+  if is_pool:
+    for pool_method in ["max_n", "90th_90th", "90th_max", "second_second", "second_max"]:
+      for max_n_pairs in [1, 2, 4, 8]:
+#    for pool_method in ["median", "median_median", "median_max", "90th_90th", "90th_max", "second_second", "second_max"]:
+#      for max_n_pairs in [1, 5, 10, 15, 20]:
+        for shortest_sentence in [1, 3, 4]:
+          yield (pool_method, max_n_pairs, shortest_sentence)
+  else:
+    yield ()
+
 def _main( ):
-  task_type = sys.argv[1]
-  corpus_name = sys.argv[2]
-  model_name = sys.argv[3]
-  lecture_list_name = sys.argv[4]
-  fea_dir = sys.argv[5]
-  dim = int(sys.argv[6])
+  argv_index = 1
+  task_type, argv_index = assign_argv(sys.argv, argv_index) # 1
+  corpus_name, argv_index = assign_argv(sys.argv, argv_index) # 2
+  model_name, argv_index = assign_argv(sys.argv, argv_index) # 3
+  lecture_list_name, argv_index = assign_argv(sys.argv, argv_index) # 4
+  fea_dir, argv_index = assign_argv(sys.argv, argv_index) # 5
+  dim, argv_index = assign_argv(sys.argv, argv_index, int) # 6
 
-  is_normalization_doc2vec = bool(int(sys.argv[7]))
-  is_normalization_cos_sim = sys.argv[8]
-  is_remove_stop = bool(int(sys.argv[9]))
-  corpus_pos_name = sys.argv[10]
-  pos_type = sys.argv[11]
+  is_normalization_doc2vec, argv_index = assign_argv(sys.argv, argv_index, lambda x: bool(int(x))) # 7
+  is_normalization_cos_sim, argv_index = assign_argv(sys.argv, argv_index) # 8
+  is_remove_stop, argv_index = assign_argv(sys.argv, argv_index, lambda x: bool(int(x))) # 9
+  corpus_pos_name, argv_index = assign_argv(sys.argv, argv_index) # 10
+  pos_type, argv_index = assign_argv(sys.argv, argv_index) # 11
 
-  corpus = load_corpus(corpus_name)
-  corpus_pos = load_corpus(corpus_pos_name)
   use_external_model = False
   fix_stem = False
   fix_math = False
@@ -309,13 +405,17 @@ def _main( ):
   is_normalization_word = False
   fix_stem_info = None
   is_pool = False
+  if 'pool' in task_type:
+    is_pool = True
+
   if re.match('.*external_model', task_type):
     model = gensim.models.Word2Vec.load_word2vec_format(model_name, binary=True)
     use_external_model = True
     if 'fix_stem' in task_type:
       fix_stem = True
-      if 'pool' in task_type:
-        is_pool = True
+
+      #if 'pool' in task_type:
+      #  is_pool = True
 
       if 'fix_stem_math' in task_type:
         fix_math = True
@@ -329,44 +429,32 @@ def _main( ):
     stop_list = load_courseware.load_stop_list()
 
   if re.match('.*textbook_lecture', task_type):
-    argv_index = 12
-    tx_list_name = sys.argv[12]
-    seg_dir = sys.argv[13]
-    tx_docs = get_tx_docs(corpus, corpus_pos, pos_type, tx_list_name)
-    if use_external_model:
-
-      if fix_stem:
-        is_normalization_word = bool(int(sys.argv[14]))
-        oov_refined = sys.argv[15]
-        stem_to_raw = sys.argv[16]
-
-
-        if fix_math:
-          math_words = sys.argv[17]
-        elif keyword_only:
-          keyword_list = sys.argv[17]
-      else:
-        oov_dict_name = sys.argv[14]
+    tx_list_name, argv_index = assign_argv(sys.argv, argv_index) # 12
+    seg_dir, argv_index = assign_argv(sys.argv, argv_index) # 13
   elif re.match('.*slides_trans', task_type):
-    window_size = int(sys.argv[12])
+    window_size, argv_index = assign_argv(sys.argv, argv_index, int) # 12
+
+  if re.match('.*textbook_lecture', task_type) or re.match('.*slides_trans', task_type):
     if use_external_model:
       if fix_stem:
-        is_normalization_word = bool(int(sys.argv[13]))
-        oov_refined = sys.argv[14]
-        stem_to_raw = sys.argv[15]
+        is_normalization_word, argv_index = assign_argv(sys.argv, argv_index, lambda x: bool(int(x))) # textbook=14/slides=13
+        oov_refined, argv_index = assign_argv(sys.argv, argv_index) # t=15/s=14
+        stem_to_raw, argv_index = assign_argv(sys.argv, argv_index) # t=16/s=15
+
         if fix_math:
-          math_words = sys.argv[16]
+          math_words, argv_index = assign_argv(sys.argv, argv_index) # t=17/s=16 ? it depends
         elif keyword_only:
-          keyword_list = sys.argv[16]
+          keyword_list, argv_index = assign_argv(sys.argv, argv_index) # t=17/s=16 ? it depends
       else:
-        oov_dict_name = sys.argv[13]
+        oov_dict_name, argv_index = assign_argv(sys.argv, argv_index) # t=14/s=13
 
   if use_external_model:
     if fix_stem:
       refined_oov = {}
       keywords = []
-      for line in open(keyword_list):
-        keywords += my_util.preprocess_content(line.strip(), stop_list, is_math=True, is_stemming=True)
+      if keyword_only:
+        for line in open(keyword_list):
+          keywords += my_util.preprocess_content(line.strip(), stop_list, is_math=True, is_stemming=True)
       keywords = list(set(keywords))
 
       for line in open(oov_refined):
@@ -400,20 +488,30 @@ def _main( ):
     else:
       oov_dict = my_util.load_stopwords(oov_dict_name)
 
-  for l_id in open(lecture_list_name):
-    l_id = l_id.strip()
-    trans_temp = get_docs(trans_doc_prefix, l_id, corpus, corpus_pos, pos_type)
-    if re.match('.*slides_trans', task_type):
-      trans = windowing(trans_temp, window_size)
-      targets = get_docs(slides_doc_prefix, l_id, corpus, corpus_pos, pos_type)
-    elif re.match('.*textbook_lecture', task_type):
-      trans = window_by_seg(trans_temp, '%s/%s' % (seg_dir, l_id))
-      targets = tx_docs
+  temp_fea_dir = fea_dir
+  for pool_param in gen_pool_param(is_pool):
+    if is_pool:
+      fea_dir = temp_fea_dir + '_%s_%d_%d' % pool_param
+      if not os.path.exists(fea_dir):
+        os.makedirs(fea_dir)
 
-    print_fea('%s/%s' % (fea_dir, l_id), trans, targets, model, dim, stop_list, is_normalization_doc2vec, is_normalization_cos_sim, is_normalization_word=is_normalization_word, fix_stem=fix_stem_info)
+    corpus = load_corpus(corpus_name, pool_param=pool_param)
+    corpus_pos = load_corpus(corpus_pos_name, pool_param=pool_param)
+
+    for l_id in open(lecture_list_name):
+      l_id = l_id.strip()
+      trans_temp = get_docs(trans_doc_prefix, l_id, corpus, corpus_pos, pos_type)
+      if re.match('.*slides_trans', task_type):
+        trans = windowing(trans_temp, window_size)
+        targets = get_docs(slides_doc_prefix, l_id, corpus, corpus_pos, pos_type)
+      elif re.match('.*textbook_lecture', task_type):
+        trans = window_by_seg(trans_temp, '%s/%s' % (seg_dir, l_id), pool_param=pool_param)
+        targets = get_tx_docs(corpus, corpus_pos, pos_type, tx_list_name)
+
+      print_fea('%s/%s' % (fea_dir, l_id), trans, targets, model, dim, stop_list, is_normalization_doc2vec, is_normalization_cos_sim, is_normalization_word=is_normalization_word, fix_stem=fix_stem_info, pool_param=pool_param)
     
-    if use_external_model and not fix_stem:
-      print_fea('%s/%s.oov' % (fea_dir, l_id), trans, targets, model, dim, stop_list, is_normalization_doc2vec, is_normalization_cos_sim, oov_dict=oov_dict)
+      if use_external_model and not fix_stem:
+        print_fea('%s/%s.oov' % (fea_dir, l_id), trans, targets, model, dim, stop_list, is_normalization_doc2vec, is_normalization_cos_sim, oov_dict=oov_dict)
 
 if __name__ == '__main__':
   _main( )
--- script/plot_sim_matrix.py
+++ script/plot_sim_matrix.py
@@ -6,6 +6,7 @@ import matplotlib
 # Force matplotlib to not use any Xwindows backend.
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import re
 import sys
 
 def quantiztion(val, quantiztion_bins=None):
@@ -16,6 +17,23 @@ def quantiztion(val, quantiztion_bins=None):
     return 1 if val > 1 else val
   else:
     return float(val)
+
+def transpose(arr):
+  return [list(x) for x in zip(*arr)]
+
+def filter_top_n(top_n_seg, cos_sim_matrix, transpose_top_n_seg):
+  if transpose_top_n_seg:
+    cos_sim_matrix = transpose(cos_sim_matrix)
+
+  for index_i, cos_sim in enumerate(cos_sim_matrix):
+    threshold = cos_sim[sorted(range(len(cos_sim)), key=lambda i: cos_sim[i])[-top_n_seg]]
+    for index_j in range(len(cos_sim)):
+      if cos_sim_matrix[index_i][index_j] < threshold:
+        cos_sim_matrix[index_i][index_j] = 0
+
+  if transpose_top_n_seg:
+    cos_sim_matrix = transpose(cos_sim_matrix)
+  return cos_sim_matrix
 
 def plot_heat_map(cos_sim_matrix, filename, is_tx):
   fig = plt.figure()
@@ -42,10 +60,18 @@ def _main( ):
   word2vec_plot_dir = sys.argv[3]
   quantiztion_bins = None
   is_tx = False
+  top_n_seg = None
+  transpose_top_n_seg = True
   if len(sys.argv) > 4:
     quantiztion_bins = int(sys.argv[4])
   if len(sys.argv) > 5:
     is_tx = bool(int(sys.argv[5]))
+  if len(sys.argv) > 6:
+    if 'h' in sys.argv[6]:
+      top_n_seg = int(re.match('(\d+)h', sys.argv[6]).group(1))
+      transpose_top_n_seg = False
+    else:
+      top_n_seg = int(sys.argv[6])
 
   if is_tx:
     cos_sim_all = []
@@ -54,14 +80,18 @@ def _main( ):
     with open('%s/%s' % (word2vec_fea_dir, l_id)) as f:
       try:
         cos_sim_matrix = [[quantiztion(y, quantiztion_bins) for y in x.strip().split('\t')] for x in f.readlines()]
+        #if top_n_seg:
+        #  cos_sim_matrix = filter_top_n(top_n_seg, cos_sim_matrix, is_tx)
         if is_tx:
           cos_sim_all += cos_sim_matrix
       except ValueError:
         print '%s/%s' % (word2vec_fea_dir, l_id)
 
-    plot_heat_map(cos_sim_matrix, '%s/%s' % (word2vec_plot_dir, l_id), is_tx)
+    #plot_heat_map(cos_sim_matrix, '%s/%s' % (word2vec_plot_dir, l_id), is_tx)
 
   if is_tx:
+    if top_n_seg:
+      cos_sim_all = filter_top_n(top_n_seg, cos_sim_all, transpose_top_n_seg)
     plot_heat_map(cos_sim_all, '%s/accumulation' % word2vec_plot_dir, is_tx)
 
 if __name__ == '__main__':
